4881b2f87ac03aa5e9908cd9b404f69c4813cea0
ABU <abubakrghazali@gmail.com>
2023-03-22 10:34:29 +0800

    insert bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { FactsComponent } from './components/facts/facts.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IntroComponent,
+    FactsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
